69e4b55a4fe972ec5cac430dc93e8f9c80707468
Shifana Saleem <shifanasaleem50@gmail.com>
2026-09-09 15:49:47 +0400

    Update server.js

--- backend/server/server.js
+++ backend/server/server.js
@@ -11,7 +11,10 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static(path.join(__dirname, "uploads"))); // Serve images
+app.use(
+  "/uploads",
+  express.static(path.join(__dirname, "../uploads"))
+);
 
 // DB Connection
 mongoose
